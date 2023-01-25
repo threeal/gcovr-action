88be77b99adc008d7fd005e18c3b177a32e6fcda
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-25 21:35:39 +0700

    add install gcovr step

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
+    core.startGroup('Install gcovr');
+    await exec.exec('pip3 install gcovr');
+    core.endGroup();
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
