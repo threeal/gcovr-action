96e50713a636b332c9f256c37f044c0b98aa052e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-01-30 15:09:44 +0700

    move `pip.installPackage` function into `deps/pip/install.ts`

--- dist/index.js
+++ dist/index.js
@@ -420,56 +420,14 @@ exports.initContext = initContext;
 /***/ }),
 
 /***/ 9875:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.installPackage = void 0;
-const core = __importStar(__nccwpck_require__(2186));
-const exec = __importStar(__nccwpck_require__(7757));
-const info_1 = __nccwpck_require__(8414);
-const cache_1 = __nccwpck_require__(143);
-async function installPackage(packageName) {
-    core.info(`Checking ${packageName}...`);
-    const pkgInfo = await (0, info_1.showPackageInfo)(packageName);
-    if (pkgInfo !== null) {
-        core.info(`Package ${packageName} already installed`);
-        return;
-    }
-    core.info(`Restoring ${packageName}...`);
-    if (await (0, cache_1.restorePackage)(packageName)) {
-        core.info(`Done restoring ${packageName}...`);
-        return;
-    }
-    await exec.exec("python3", ["-m", "pip", "install", "--user", packageName]);
-    core.info(`Caching ${packageName}...`);
-    await (0, cache_1.cachePackage)(packageName);
-}
-exports.installPackage = installPackage;
+var install_1 = __nccwpck_require__(1450);
+Object.defineProperty(exports, "installPackage", ({ enumerable: true, get: function () { return install_1.installPackage; } }));
 
 
 /***/ }),
@@ -528,6 +486,61 @@ async function showPackageInfo(packageName) {
     };
 }
 exports.showPackageInfo = showPackageInfo;
+
+
+/***/ }),
+
+/***/ 1450:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.installPackage = void 0;
+const core = __importStar(__nccwpck_require__(2186));
+const exec = __importStar(__nccwpck_require__(7757));
+const info_1 = __nccwpck_require__(8414);
+const cache_1 = __nccwpck_require__(143);
+async function installPackage(packageName) {
+    core.info(`Checking ${packageName}...`);
+    const pkgInfo = await (0, info_1.showPackageInfo)(packageName);
+    if (pkgInfo !== null) {
+        core.info(`Package ${packageName} already installed`);
+        return;
+    }
+    core.info(`Restoring ${packageName}...`);
+    if (await (0, cache_1.restorePackage)(packageName)) {
+        core.info(`Done restoring ${packageName}...`);
+        return;
+    }
+    await exec.exec("python3", ["-m", "pip", "install", "--user", packageName]);
+    core.info(`Caching ${packageName}...`);
+    await (0, cache_1.cachePackage)(packageName);
+}
+exports.installPackage = installPackage;
 
 
 /***/ }),
