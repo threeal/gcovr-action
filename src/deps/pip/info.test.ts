b436dc6904a7e7a4d63ddb5741ca67b9b2a3f4f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-02 18:06:24 +0700

    modify test flow for `pip.showPackageInfo`

--- src/deps/pip/info.test.ts
+++ src/deps/pip/info.test.ts
@@ -1,7 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
 import * as fs from "fs";
-import * as path from "path";
-import { showPackageInfo } from "./info";
+import { PackageInfo, showPackageInfo } from "./info";
 
 function expectPathExist(path: string) {
   if (!fs.existsSync(path)) {
@@ -9,29 +8,54 @@ function expectPathExist(path: string) {
   }
 }
 
-describe("pip module", () => {
-  test("show pip package info", async () => {
-    const pkgInfo = await showPackageInfo("pip");
-    expect(pkgInfo).not.toBeNull();
-    if (pkgInfo === null) {
-      throw new Error("`showPackageInfo` should not return null");
-    }
-    expect(pkgInfo.name).toBe("pip");
-    expect(pkgInfo.version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
-    expectPathExist(pkgInfo.location);
-    const dirs = pkgInfo.directories();
-    expect(dirs.length).toBeGreaterThan(0);
-    for (const dir of dirs) {
-      expectPathExist(dir);
-    }
-    const execs = await pkgInfo.executables();
-    expect(execs.length).toBeGreaterThan(0);
-    for (const exec of execs) {
-      expectPathExist(exec);
-    }
+describe("test show info of a pip package", () => {
+  describe("show info of a valid package (pip)", () => {
+    const pkgInfo = showPackageInfo("pip");
+    test("should not return null", async () => {
+      expect(await pkgInfo).not.toBeNull();
+    });
+
+    describe("check contents of a package info (pip info)", () => {
+      const info = pkgInfo as Promise<PackageInfo>;
+
+      test("name should be valid", async () => {
+        expect((await info).name).toBe("pip");
+      });
+      test("version should be valid", async () => {
+        expect((await info).version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+      });
+      test("location should be exist", async () => {
+        expectPathExist((await info).location);
+      });
+      test("dependencies should be valid", async () => {
+        expect((await info).dependencies.length).toBe(0);
+      });
+      test("files should be valid", async () => {
+        expect((await info).files.length).toBeGreaterThan(0);
+      });
+
+      test("directories should be exist", async () => {
+        const dirs = (await info).directories();
+        expect(dirs.length).toBeGreaterThan(0);
+        for (const dir of dirs) {
+          expectPathExist(dir);
+        }
+      });
+
+      test("executables should be exist", async () => {
+        const execs = await (await info).executables();
+        expect(execs.length).toBeGreaterThan(0);
+        for (const exec of execs) {
+          expectPathExist(exec);
+        }
+      });
+    });
   });
-  test("show invalid package info return null", async () => {
-    const pkgInfo = await showPackageInfo("an-invalid-package");
-    expect(pkgInfo).toBeNull();
+
+  describe("show info of an invalid package", () => {
+    const pkgInfo = showPackageInfo("an-invalid-package");
+    test("should return null", async () => {
+      expect(await pkgInfo).toBeNull();
+    });
   });
 });
