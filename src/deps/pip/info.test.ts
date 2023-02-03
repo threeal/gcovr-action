b3143dfd209dd0946239aa3754a3d772ae95ec25
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 17:06:38 +0700

    audit tests order

--- src/deps/pip/info.test.ts
+++ src/deps/pip/info.test.ts
@@ -20,77 +20,73 @@ function appendInfo(err: unknown, info: { [key: string]: any }): unknown {
 }
 
 describe("test show info of a pip package", () => {
-  describe("test show info of a valid package (pip)", () => {
-    describe("show the package info", () => {
-      const pkgInfo = showPackageInfo("pip");
-      test("should be valid", async () => {
-        await expect(pkgInfo).resolves.toBeInstanceOf(PackageInfo);
+  describe("show info of a valid package (pip)", () => {
+    let pkgInfo: PackageInfo;
+    test("should be valid", async () => {
+      const res = showPackageInfo("pip");
+      await expect(res).resolves.toBeInstanceOf(PackageInfo);
+      pkgInfo = (await res) as PackageInfo;
+    });
+
+    describe("check contents of the package info", () => {
+      test("name should be valid", () => {
+        expect(pkgInfo.name).toBe("pip");
+      });
+      test("version should be valid", () => {
+        expect(pkgInfo.version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+      });
+      test("location should be exist", () => {
+        expectPathExist(pkgInfo.location);
       });
 
-      describe("check contents of the package info", () => {
-        const info = pkgInfo as Promise<PackageInfo>;
+      test("dependencies should be valid", () => {
+        const deps = pkgInfo.dependencies;
+        try {
+          expect(deps.length).toBe(0);
+        } catch (err) {
+          throw appendInfo(err, { deps: deps });
+        }
+      });
 
-        test("name should be valid", async () => {
-          expect((await info).name).toBe("pip");
-        });
-        test("version should be valid", async () => {
-          expect((await info).version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
-        });
-        test("location should be exist", async () => {
-          expectPathExist((await info).location);
-        });
+      test("files should be valid", () => {
+        const files = pkgInfo.files;
+        try {
+          expect(files.length).toBeGreaterThan(0);
+        } catch (err) {
+          throw appendInfo(err, { files: files });
+        }
+      });
 
-        test("dependencies should be valid", async () => {
-          const deps = (await info).dependencies;
-          try {
-            expect(deps.length).toBe(0);
-          } catch (err) {
-            throw appendInfo(err, { deps: deps });
+      test("directories should be exist", () => {
+        const dirs = pkgInfo.directories();
+        try {
+          expect(dirs.length).toBe(2);
+          for (const dir of dirs) {
+            expectPathExist(dir);
           }
-        });
+        } catch (err) {
+          throw appendInfo(err, { dirs: dirs });
+        }
+      });
 
-        test("files should be valid", async () => {
-          const files = (await info).files;
-          try {
-            expect(files.length).toBeGreaterThan(0);
-          } catch (err) {
-            throw appendInfo(err, { files: files });
+      test("executables should be exist", async () => {
+        const execs = await pkgInfo.executables();
+        try {
+          expect(execs.length).toBe(os.type() === "Windows_NT" ? 4 : 3);
+          for (const exec of execs) {
+            expectPathExist(exec);
           }
-        });
-
-        test("directories should be exist", async () => {
-          const dirs = (await info).directories();
-          try {
-            expect(dirs.length).toBe(2);
-            for (const dir of dirs) {
-              expectPathExist(dir);
-            }
-          } catch (err) {
-            throw appendInfo(err, { dirs: dirs });
-          }
-        });
-
-        test("executables should be exist", async () => {
-          const execs = await (await info).executables();
-          try {
-            expect(execs.length).toBe(os.type() === "Windows_NT" ? 4 : 3);
-            for (const exec of execs) {
-              expectPathExist(exec);
-            }
-          } catch (err) {
-            throw appendInfo(err, { execs: execs });
-          }
-        });
+        } catch (err) {
+          throw appendInfo(err, { execs: execs });
+        }
       });
     });
   });
 
-  describe("test show info of an invalid package", () => {
-    describe("show the package info", () => {
+  describe("show info of an invalid package", () => {
+    test("should be null", async () => {
       const pkgInfo = showPackageInfo("an-invalid-package");
-      test("should be null", async () => {
-        await expect(pkgInfo).resolves.toBeNull();
-      });
+      await expect(pkgInfo).resolves.toBeNull();
     });
   });
 });
--- src/deps/pip/install.test.ts
+++ src/deps/pip/install.test.ts
@@ -3,28 +3,24 @@ import { PackageInfo, showPackageInfo } from "./info";
 import { installPackage } from "./install";
 
 describe("test install a pip package", () => {
-  describe("test install a valid package (requests)", () => {
-    describe("install the package", () => {
+  describe("install a valid package (requests)", () => {
+    test("should be resolved", async () => {
       const res = installPackage("requests");
-      test("should be resolved", async () => {
-        await expect(res).resolves.toBeUndefined();
-      });
+      await expect(res).resolves.toBeUndefined();
     });
 
     describe("show the package info", () => {
-      const pkgInfo = showPackageInfo("pip");
       test("should be valid", async () => {
-        await expect(pkgInfo).resolves.toBeInstanceOf(PackageInfo);
+        const res = showPackageInfo("pip");
+        await expect(res).resolves.toBeInstanceOf(PackageInfo);
       });
     });
   });
 
-  describe("test install an invalid package", () => {
-    describe("install the package", () => {
+  describe("install an invalid package", () => {
+    test("should be rejected", async () => {
       const res = installPackage("an-invalid-package");
-      test("should be rejected", async () => {
-        await expect(res).rejects.toThrow();
-      });
+      await expect(res).rejects.toThrow();
     });
   });
 });
