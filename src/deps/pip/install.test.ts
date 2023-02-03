3235d75a998c5724235948c44a50837cc0e33973
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 15:52:33 +0700

    modify test flow in the `deps/pip/install.test.ts`

--- src/deps/pip/install.test.ts
+++ src/deps/pip/install.test.ts
@@ -19,10 +19,12 @@ describe("test install a pip package", () => {
     });
   });
 
-  describe("install an invalid package", () => {
-    const res = installPackage("an-invalid-package");
-    test("should return a rejected promise", async () => {
-      await expect(res).rejects.toThrow();
+  describe("test install an invalid package", () => {
+    describe("install the package", () => {
+      const res = installPackage("an-invalid-package");
+      test("should be rejected", async () => {
+        await expect(res).rejects.toThrow();
+      });
     });
   });
 });
