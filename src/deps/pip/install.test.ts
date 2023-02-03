9eeb8fb38fbfa26ceddf3720919b28d8f9dcd9fb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 17:32:26 +0700

    test uninstall a pip package

--- src/deps/pip/install.test.ts
+++ src/deps/pip/install.test.ts
@@ -14,7 +14,7 @@ describe("test install a pip package", () => {
     };
 
     const testShowPackageInfo = async () => {
-      const res = showPackageInfo("pip");
+      const res = showPackageInfo("requests");
       await expect(res).resolves.toBeInstanceOf(PackageInfo);
     };
 
@@ -37,6 +37,45 @@ describe("test install a pip package", () => {
     test("should be rejected", async () => {
       const res = installPackage("an-invalid-package");
       await expect(res).rejects.toThrow();
+    });
+  });
+});
+
+describe("test uninstall a pip package", () => {
+  describe("uninstall a valid package (requests)", () => {
+    beforeAll(async () => {
+      await installPackage("requests");
+    });
+
+    const testUninstallPackage = async () => {
+      const res = uninstallPackage("requests");
+      await expect(res).resolves.toBeUndefined();
+    };
+
+    const testShowPackageInfo = async () => {
+      const res = showPackageInfo("requests");
+      await expect(res).resolves.toBeNull();
+    };
+
+    test("should be resolved", testUninstallPackage);
+
+    describe("show the package info", () => {
+      test("should be null", testShowPackageInfo);
+    });
+
+    describe("uninstall the package again", () => {
+      test("should be resolved", testUninstallPackage);
+    });
+
+    describe("show the package info again", () => {
+      test("should be null", testShowPackageInfo);
+    });
+  });
+
+  describe("uninstall an invalid package", () => {
+    test("should be resolved", async () => {
+      const res = uninstallPackage("an-invalid-package");
+      await expect(res).resolves.toBeUndefined();
     });
   });
 });
