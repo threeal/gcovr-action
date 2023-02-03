a4441fd9f571c17c99eaec50e36614f24339a2d8
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 17:24:33 +0700

    test pip install an installed package

--- src/deps/pip/install.test.ts
+++ src/deps/pip/install.test.ts
@@ -1,23 +1,35 @@
-import { beforeEach, describe, expect, test } from "@jest/globals";
+import { beforeAll, describe, expect, test } from "@jest/globals";
 import { PackageInfo, showPackageInfo } from "./info";
 import { installPackage, uninstallPackage } from "./install";
 
 describe("test install a pip package", () => {
   describe("install a valid package (requests)", () => {
-    beforeEach(async () => {
+    beforeAll(async () => {
       await uninstallPackage("requests");
     });
 
-    test("should be resolved", async () => {
+    const testInstallPackage = async () => {
       const res = installPackage("requests");
       await expect(res).resolves.toBeUndefined();
-    });
+    };
+
+    const testShowPackageInfo = async () => {
+      const res = showPackageInfo("pip");
+      await expect(res).resolves.toBeInstanceOf(PackageInfo);
+    };
+
+    test("should be resolved", testInstallPackage);
 
     describe("show the package info", () => {
-      test("should be valid", async () => {
-        const res = showPackageInfo("pip");
-        await expect(res).resolves.toBeInstanceOf(PackageInfo);
-      });
+      test("should be valid", testShowPackageInfo);
+    });
+
+    describe("install the package again", () => {
+      test("should be resolved", testInstallPackage);
+    });
+
+    describe("show the package info again", () => {
+      test("should be valid", testShowPackageInfo);
     });
   });
 
