c5cd66048b3adf632d149d042a00fe70ecb042da
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 16:08:10 +0700

    modify test flow in the `deps/pip/info.test.ts`

--- src/deps/pip/info.test.ts
+++ src/deps/pip/info.test.ts
@@ -20,73 +20,77 @@ function appendInfo(err: unknown, info: { [key: string]: any }): unknown {
 }
 
 describe("test show info of a pip package", () => {
-  describe("show info of a valid package (pip)", () => {
-    const pkgInfo = showPackageInfo("pip");
-    test("should not return null", async () => {
-      expect(await pkgInfo).not.toBeNull();
-    });
-
-    describe("check contents of a package info (pip info)", () => {
-      const info = pkgInfo as Promise<PackageInfo>;
-
-      test("name should be valid", async () => {
-        expect((await info).name).toBe("pip");
-      });
-      test("version should be valid", async () => {
-        expect((await info).version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
-      });
-      test("location should be exist", async () => {
-        expectPathExist((await info).location);
+  describe("test show info of a valid package (pip)", () => {
+    describe("show the package info", () => {
+      const pkgInfo = showPackageInfo("pip");
+      test("should be valid", async () => {
+        await expect(pkgInfo).resolves.toBeInstanceOf(PackageInfo);
       });
 
-      test("dependencies should be valid", async () => {
-        const deps = (await info).dependencies;
-        try {
-          expect(deps.length).toBe(0);
-        } catch (err) {
-          throw appendInfo(err, { deps: deps });
-        }
-      });
+      describe("check contents of the package info", () => {
+        const info = pkgInfo as Promise<PackageInfo>;
 
-      test("files should be valid", async () => {
-        const files = (await info).files;
-        try {
-          expect(files.length).toBeGreaterThan(0);
-        } catch (err) {
-          throw appendInfo(err, { files: files });
-        }
-      });
+        test("name should be valid", async () => {
+          expect((await info).name).toBe("pip");
+        });
+        test("version should be valid", async () => {
+          expect((await info).version).toMatch(/^(\d+\.)?(\d+\.)?(\*|\d+)$/);
+        });
+        test("location should be exist", async () => {
+          expectPathExist((await info).location);
+        });
 
-      test("directories should be exist", async () => {
-        const dirs = (await info).directories();
-        try {
-          expect(dirs.length).toBe(2);
-          for (const dir of dirs) {
-            expectPathExist(dir);
+        test("dependencies should be valid", async () => {
+          const deps = (await info).dependencies;
+          try {
+            expect(deps.length).toBe(0);
+          } catch (err) {
+            throw appendInfo(err, { deps: deps });
           }
-        } catch (err) {
-          throw appendInfo(err, { dirs: dirs });
-        }
-      });
+        });
 
-      test("executables should be exist", async () => {
-        const execs = await (await info).executables();
-        try {
-          expect(execs.length).toBe(os.type() === "Windows_NT" ? 4 : 3);
-          for (const exec of execs) {
-            expectPathExist(exec);
+        test("files should be valid", async () => {
+          const files = (await info).files;
+          try {
+            expect(files.length).toBeGreaterThan(0);
+          } catch (err) {
+            throw appendInfo(err, { files: files });
           }
-        } catch (err) {
-          throw appendInfo(err, { execs: execs });
-        }
+        });
+
+        test("directories should be exist", async () => {
+          const dirs = (await info).directories();
+          try {
+            expect(dirs.length).toBe(2);
+            for (const dir of dirs) {
+              expectPathExist(dir);
+            }
+          } catch (err) {
+            throw appendInfo(err, { dirs: dirs });
+          }
+        });
+
+        test("executables should be exist", async () => {
+          const execs = await (await info).executables();
+          try {
+            expect(execs.length).toBe(os.type() === "Windows_NT" ? 4 : 3);
+            for (const exec of execs) {
+              expectPathExist(exec);
+            }
+          } catch (err) {
+            throw appendInfo(err, { execs: execs });
+          }
+        });
       });
     });
   });
 
-  describe("show info of an invalid package", () => {
-    const pkgInfo = showPackageInfo("an-invalid-package");
-    test("should return null", async () => {
-      expect(await pkgInfo).toBeNull();
+  describe("test show info of an invalid package", () => {
+    describe("show the package info", () => {
+      const pkgInfo = showPackageInfo("an-invalid-package");
+      test("should be null", async () => {
+        await expect(pkgInfo).resolves.toBeNull();
+      });
     });
   });
 });
