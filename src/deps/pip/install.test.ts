4adf26453f81e681f91305b30cfbe594ec12b28f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-03 15:48:50 +0700

    test install a valid pip package (requests)

--- src/deps/pip/install.test.ts
+++ src/deps/pip/install.test.ts
@@ -1,7 +1,24 @@
 import { describe, expect, test } from "@jest/globals";
+import { PackageInfo, showPackageInfo } from "./info";
 import { installPackage } from "./install";
 
 describe("test install a pip package", () => {
+  describe("test install a valid package (requests)", () => {
+    describe("install the package", () => {
+      const res = installPackage("requests");
+      test("should be resolved", async () => {
+        await expect(res).resolves.toBeUndefined();
+      });
+    });
+
+    describe("show the package info", () => {
+      const pkgInfo = showPackageInfo("pip");
+      test("should be valid", async () => {
+        await expect(pkgInfo).resolves.toBeInstanceOf(PackageInfo);
+      });
+    });
+  });
+
   describe("install an invalid package", () => {
     const res = installPackage("an-invalid-package");
     test("should return a rejected promise", async () => {
