0687e705c9cd5aad0d2fc29d72cc0b0bf5f1b77c
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-08 16:42:11 +0700

    add utility functions to simplify creation of jest's expect extended functions

--- src/testing/expect.ts
+++ src/testing/expect.ts
@@ -3,18 +3,30 @@ import * as fs from "fs";
 
 const expect = Object.assign(jestExpect);
 
+interface Result {
+  message: () => string;
+  pass: boolean;
+}
+
+function pass(message: string): Result {
+  return {
+    message: () => message,
+    pass: true,
+  };
+}
+
+function notPass(message: string): Result {
+  return {
+    message: () => message,
+    pass: false,
+  };
+}
+
 expect.extend({
-  toBeExist(received: string) {
-    if (fs.existsSync(received)) {
-      return {
-        message: () => `expected path '${received}' to be exist`,
-        pass: true,
-      };
-    }
-    return {
-      message: () => `expected path ${received} not to be exist`,
-      pass: false,
-    };
+  toBeExist(received: string): Result {
+    return fs.existsSync(received)
+      ? pass(`expected path '${received}' to be exist`)
+      : notPass(`expected path ${received} not to be exist`);
   },
 });
 
