c2f4232224680f2271b1791ef799dc4adbe627e6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-11 21:18:07 +0700

    move mock for `ansi-styles` to `src/testing/mock`

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   setupFilesAfterEnv: ["./jest.setup.js"],
   moduleNameMapper: {
     'actions/cache': '<rootDir>/lib/testing/mock/actions-cache.js',
-    'ansi-styles': '<rootDir>/mock/ansi-styles.js',
+    'ansi-styles': '<rootDir>/lib/testing/mock/ansi-styles.js',
   },
   verbose: true
 }
