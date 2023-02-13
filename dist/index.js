bae3569bdac0f838c5d18e47ad3a65e7a1fdeda2
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-02-13 20:53:17 +0700

    remove old pip cache implementation

--- dist/index.js
+++ dist/index.js
@@ -339,13 +339,12 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.restorePackage = exports.cachePackage = exports.PackageContentCacheInfo = exports.PackageCacheInfo = void 0;
+exports.PackageContentCacheInfo = exports.PackageCacheInfo = void 0;
 const cache = __importStar(__nccwpck_require__(7799));
 const fs = __importStar(__nccwpck_require__(7147));
 const os = __importStar(__nccwpck_require__(2037));
 const path = __importStar(__nccwpck_require__(1017));
 const io = __importStar(__nccwpck_require__(3709));
-const context_1 = __nccwpck_require__(3272);
 const info_1 = __nccwpck_require__(8414);
 class PackageCacheInfo {
     constructor(packageName) {
@@ -419,77 +418,6 @@ class PackageContentCacheInfo {
     }
 }
 exports.PackageContentCacheInfo = PackageContentCacheInfo;
-async function getCacheInfo(packageName) {
-    const context = await (0, context_1.initContext)();
-    return {
-        paths: [
-            path.join(context.userSitePackage, `${packageName.toLowerCase()}*`),
-            path.join(context.userSitePackage, `${packageName}*`),
-        ],
-        key: `pip-${os.type()}-${packageName}`,
-    };
-}
-async function cachePackage(packageName) {
-    const info = await getCacheInfo(packageName);
-    await cache.saveCache(info.paths, info.key);
-}
-exports.cachePackage = cachePackage;
-async function restorePackage(packageName) {
-    const info = await getCacheInfo(packageName);
-    const key = await cache.restoreCache(info.paths, info.key);
-    return key !== undefined;
-}
-exports.restorePackage = restorePackage;
-
-
-/***/ }),
-
-/***/ 3272:
-/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
-
-"use strict";
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.initContext = void 0;
-const exec = __importStar(__nccwpck_require__(7757));
-let gContext = null;
-async function getUserSitePackages() {
-    const cmd = "import site; print(site.getusersitepackages())";
-    const out = await exec.execOut("python3", ["-c", cmd]);
-    return out.trim();
-}
-async function initContext() {
-    if (gContext === null) {
-        gContext = {
-            userSitePackage: await getUserSitePackages(),
-        };
-    }
-    return gContext;
-}
-exports.initContext = initContext;
 
 
 /***/ }),
--- src/deps/pip/cache.ts
+++ src/deps/pip/cache.ts
@@ -3,7 +3,6 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 import * as io from "../../io";
-import { initContext } from "./context";
 import { showPackageInfo } from "./info";
 
 export class PackageCacheInfo {
@@ -88,31 +87,4 @@ export class PackageContentCacheInfo {
   async restore(): Promise<string | undefined> {
     return await cache.restoreCache(this.paths, this.key);
   }
-}
-
-interface CacheInfo {
-  paths: string[];
-  key: string;
-}
-
-async function getCacheInfo(packageName: string): Promise<CacheInfo> {
-  const context = await initContext();
-  return {
-    paths: [
-      path.join(context.userSitePackage, `${packageName.toLowerCase()}*`),
-      path.join(context.userSitePackage, `${packageName}*`),
-    ],
-    key: `pip-${os.type()}-${packageName}`,
-  };
-}
-
-export async function cachePackage(packageName: string): Promise<void> {
-  const info = await getCacheInfo(packageName);
-  await cache.saveCache(info.paths, info.key);
-}
-
-export async function restorePackage(packageName: string): Promise<boolean> {
-  const info = await getCacheInfo(packageName);
-  const key = await cache.restoreCache(info.paths, info.key);
-  return key !== undefined;
 }
