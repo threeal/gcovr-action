35883dd5d1877f3e177afc990e7e93e3117bcb22
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-26 19:49:14 +0700

    test(eslint): remove importing config from `@actions-kit/dev`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
-const dev = require("@actions-kit/dev");
-
-module.exports = dev.createEslintConfig();
+module.exports = {
+  plugins: ["@typescript-eslint", "jest"],
+  extends: ["plugin:github/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 9,
+    project: "tsconfig.eslint.json",
+    sourceType: "module",
+  },
+  rules: {
+    camelcase: "off",
+    "i18n-text/no-en": "off",
+    "import/no-namespace": "off",
+    "no-shadow": "off",
+  },
+  env: {
+    es6: true,
+    "jest/globals": true,
+    node: true,
+  },
+  ignorePatterns: ["jest.config.ts", "lib/"],
+};
