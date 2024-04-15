a39a24502d6733bf2a0cd254cc1b407bad2681db
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-15 20:20:02 +0700

    refactor: remove `run` function and move its content to top level

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,13 @@ import * as coveralls from "./coveralls.js";
 import * as deps from "./deps/index.js";
 import * as gcovr from "./gcovr.js";
 
-async function run(): Promise<void> {
-  try {
-    const inputs = action.processInputs();
-    await deps.check();
-    await gcovr.run(inputs);
-    if (inputs.coverallsSend && inputs.coverallsOut.length > 0) {
-      await coveralls.send(inputs.coverallsOut);
-    }
-  } catch (err) {
-    core.setFailed(err as Error);
+try {
+  const inputs = action.processInputs();
+  await deps.check();
+  await gcovr.run(inputs);
+  if (inputs.coverallsSend && inputs.coverallsOut.length > 0) {
+    await coveralls.send(inputs.coverallsOut);
   }
+} catch (err) {
+  core.setFailed(err as Error);
 }
-
-run();
